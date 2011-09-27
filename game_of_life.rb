9a43ffd4b2e916c51f3b20dd19313d885aea68a3
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-27 10:12:20 -0400

    Add test setup for TestSpec

--- game_of_life.rb
+++ game_of_life.rb
@@ -12,3 +12,26 @@ class Runner
   end
 
 end
+
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe Runner do
+  before do
+    @start = [%w{X . X},
+              %w{. X .},
+              %w{X . X}]
+  
+    @end = [%w{. X .},
+            %w{X . X},
+            %w{. X .}]
+
+    @game = Runner.new @start
+  end
+
+  it "takes the next step" do
+    @game.next_step
+
+    @game.board.must_equal @end
+  end
+end
