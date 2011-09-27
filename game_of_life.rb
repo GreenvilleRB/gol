566d29129ee0cced96ef376a3b9a9a4ad22fefb8
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-27 14:22:26 -0400

    Set up basic testing environment with Riot

--- game_of_life.rb
+++ game_of_life.rb
@@ -12,3 +12,30 @@ class Runner
   end
 
 end
+
+require 'riot'
+require 'riot/rr'
+
+context "Runner" do
+  setup do
+    @start = [%w{X . X},
+              %w{. X .},
+              %w{X . X}]
+
+    @end = [%w{. X .},
+            %w{X . X},
+            %w{. X .}]
+
+    Runner.new(@start)
+  end
+
+  context "takes the next step" do
+    hookup { topic.next_step }
+
+    asserts(:board).equals @end
+  end
+end
+
+__END__
+If you need to use mocks or stubs, checkout out RR:
+https://github.com/btakita/rr
