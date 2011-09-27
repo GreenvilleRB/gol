93ab9787fab363aedb6c3fe6beaf393a9d089f9b
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-27 10:10:16 -0400

    Set up basic testing environment with TestUnit

--- game_of_life.rb
+++ game_of_life.rb
@@ -12,3 +12,31 @@ class Runner
   end
 
 end
+
+require 'test/unit'
+
+class RunnerTest < Test::Unit::TestCase
+  def setup
+    @start = [%w{X . X},
+              %w{. X .},
+              %w{X . X}]
+
+    @end = [%w{. X .},
+            %w{X . X},
+            %w{. X .}]
+
+    @game = Runner.new @start
+  end
+
+  def teardown
+    @start = nil
+    @end = nil
+    @game = nil
+  end
+
+  def test_taking_the_next_step
+    @game.next_step
+
+    assert_equal(@end, @game.board)
+  end
+end
