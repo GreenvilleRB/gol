078e4cafb8c2babd905705722b19bb80063a8420
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-27 10:06:47 -0400

    Add basic game runner class

---
+++ game_of_life.rb
@@ -0,0 +1,14 @@
+class Runner
+  
+  attr_reader :board
+
+  # Represent living cells with 'X', dead ones with a .
+  def initialize(array)
+    @board = array
+  end
+
+  def next_step
+
+  end
+
+end
