e0159beeea27ddaf0533e946624a7e0020588f79
Rick Harris <rick.c.harris@gmail.com>
2011-11-16 00:48:20 -0500

    Add tested step function

--- game_of_life.rb
+++ game_of_life.rb
@@ -15,6 +15,29 @@ class Runner
     @board = array
   end
 
+  def step
+
+    new_board = Marshal.load(Marshal.dump(@board))
+
+    @board.each.with_index do |row, i|
+      row.each.with_index do |col, j|
+
+        living_siblings = number_living_siblings(i, j)
+
+        if col == 'X' && (living_siblings < 2 || living_siblings > 3)
+          new_board[i][j] = '.'
+        end
+
+        if col == '.' && living_siblings == 3
+          new_board[i][j] = 'X'
+        end
+
+      end
+    end
+
+    @board = new_board
+  end
+
   def number_living_siblings(row, col)
 
     living_siblings = 0
@@ -23,13 +46,13 @@ class Runner
     range.each do |i|
       range.each do |j|
 
-        neighbor = { :row => row + i, :col => col + j }
-        is_self = (neighbor[:row] == row) && (neighbor[:col] == col)
-        is_row_valid = neighbor[:row] >= 0 && neighbor[:row] < @board.length
-        is_col_valid = neighbor[:col] >= 0 && neighbor[:col] < @board[row].length
+        sibling = { :row => row + i, :col => col + j }
+        is_self = (sibling[:row] == row) && (sibling[:col] == col)
+        is_row_valid = sibling[:row] >= 0 && sibling[:row] < @board.length
+        is_col_valid = sibling[:col] >= 0 && sibling[:col] < @board[row].length
         is_valid_coordinate = is_row_valid && is_col_valid
 
-        if !is_self && is_valid_coordinate && @board[neighbor[:row]][neighbor[:col]] == 'X'
+        if !is_self && is_valid_coordinate && @board[sibling[:row]][sibling[:col]] == 'X'
           living_siblings += 1
         end
       end
@@ -43,21 +66,33 @@ end
 require 'test/unit'
 
 class RunnerTest < Test::Unit::TestCase
+
+  def setup
+    @start = [%w{X . X},
+              %w{X . .},
+              %w{X X X}]
+
+    @end   = [%w{. X .},
+              %w{X . X},
+              %w{X X .}]
+
+    @game = Runner.new @start
+  end
+
   def test_number_of_living_neighbors
-    board = [%w{X . X},
-             %w{X . .},
-             %w{X X X}]
+    assert_equal 1, @game.number_living_siblings(0, 0)
+    assert_equal 3, @game.number_living_siblings(0, 1)
+    assert_equal 0, @game.number_living_siblings(0, 2)
+    assert_equal 3, @game.number_living_siblings(1, 0)
+    assert_equal 6, @game.number_living_siblings(1, 1)
+    assert_equal 3, @game.number_living_siblings(1, 2)
+    assert_equal 2, @game.number_living_siblings(2, 0)
+    assert_equal 3, @game.number_living_siblings(2, 1)
+    assert_equal 1, @game.number_living_siblings(2, 2)
+  end
 
-    game = Runner.new board
-
-    assert_equal 1, game.number_living_siblings(0, 0)
-    assert_equal 3, game.number_living_siblings(0, 1)
-    assert_equal 0, game.number_living_siblings(0, 2)
-    assert_equal 3, game.number_living_siblings(1, 0)
-    assert_equal 6, game.number_living_siblings(1, 1)
-    assert_equal 3, game.number_living_siblings(1, 2)
-    assert_equal 2, game.number_living_siblings(2, 0)
-    assert_equal 3, game.number_living_siblings(2, 1)
-    assert_equal 1, game.number_living_siblings(2, 2)
+  def test_step
+    @game.step
+    assert_equal @end, @game.board
   end
 end
